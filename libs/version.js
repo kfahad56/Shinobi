2a91db80247d148eb20daeccb3979ffd5bba04a1
kfahad56 <fahad@devomark.com>
2021-01-12 11:55:28 +0000

    sync

--- libs/version.js
+++ libs/version.js
@@ -1,19 +1,22 @@
 var exec = require('child_process').exec
-module.exports = function(s,config,lang,app,io){
-    var getRepositoryCommitId = function(callback){
-        exec(`git rev-parse HEAD`,function(err,response){
-            if(response){
+module.exports = function (s, config, lang, app, io) {
+    var getRepositoryCommitId = function (callback) {
+        exec(`git rev-parse HEAD`, function (err, response) {
+            if (response) {
                 var data = response.toString()
                 var isGitRespository = false
-                if(data.indexOf('not a git repository') === -1){
+                if (data.indexOf('not a git repository') === -1) {
                     s.currentVersion = data
                     isGitRespository = true
-                    s.systemLog(`Current Version : ${s.currentVersion}`)
+                    s.systemLog("")
+                    console.log("s.currentVersion")
+                    console.log(s.currentVersion)
+                    // s.systemLog(`Current Version : ${s.currentVersion}`)
                 }
-            }else if(err){
+            } else if (err) {
                 s.debugLog('Git is not installed.')
             }
-            if(callback)callback(!isGitRespository,data)
+            if (callback) callback(!isGitRespository, data)
         })
     }
     getRepositoryCommitId()
